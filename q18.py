572ec7efddab49fb181ac69a3d32e5967cd2bdf0
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:16:32 +0530

    Question 18

---
+++ q18.py
@@ -0,0 +1,4 @@
+# Format a string using f-strings (or format()) to display user info (e.g., name, age).
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old.")
