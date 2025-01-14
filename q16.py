f03f978a30e06fe2e7773ab561ffc2fe56b0dad4
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:14:45 +0530

    Question 16

---
+++ q16.py
@@ -0,0 +1,5 @@
+# Compute x raised to the power of y without using built-in functions (like pow).
+x = float(input("Enter base: "))
+y = int(input("Enter exponent: "))
+print(x, "raised to the power of", y, "is:", x**y)
+
