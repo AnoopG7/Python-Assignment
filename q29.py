c983710821b817777fd27a0cb056a77e9b78e085
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:35:51 +0530

    Question 29

---
+++ q29.py
@@ -0,0 +1,4 @@
+# Convert a given distance in miles to kilometers and print the result.
+miles = float(input("Enter distance in miles: "))
+kilometers = miles * 1.60934
+print(f"The distance in kilometers is: {kilometers}")
