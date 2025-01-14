d1db174f0d7e0d31d65391332b16c72ca3d9c89b
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:02:52 +0530

    Question 3

---
+++ q3.py
@@ -0,0 +1,4 @@
+# Prompt the user for two numbers and print their sum.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("The sum is: ", (num1 + num2))
