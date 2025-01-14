a258c1883b8b07a472c6e2e6227e4337447e78bb
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:35:07 +0530

    Question 28

---
+++ q28.py
@@ -0,0 +1,4 @@
+# Prompt the user for a number (as a string) and compute the sum of its digits.
+number = input("Enter a number: ")
+sum_digits = sum(int(digit) for digit in number)
+print("Sum of digits:", sum_digits)
