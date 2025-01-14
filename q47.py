eadd4f0b69c18b1796c1277f97a18d4db581091c
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 01:37:56 +0530

    Question 47

---
+++ q47.py
@@ -0,0 +1,9 @@
+# Given two sets, demonstrate union, intersection, and difference.
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+union_set = set1 | set2
+intersection_set = set1 & set2
+difference_set = set1 - set2
+print("Union:", union_set)
+print("Intersection:", intersection_set)
+print("Difference:", difference_set)
