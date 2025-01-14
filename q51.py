a05fe051bcd222b928f74a6cfafc530d72067dab
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 01:49:04 +0530

    Delete q51.py

--- q51.py
+++
@@ -1,7 +0,0 @@
-# Write a program that reads a text file and prints its contents to the screen.
-filename = input("Enter the filename to read from: ")
-try:
-    with open(filename, 'r') as file:
-        print(file.read())
-except FileNotFoundError:
-    print("The file was not found. Please check the filename and path.")
