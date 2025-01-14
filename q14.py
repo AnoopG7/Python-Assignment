941676791e9ffd98bce47cb7479df33a2274b684
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:11:40 +0530

    Question 14

---
+++ q14.py
@@ -0,0 +1,11 @@
+# Given a string, count how many vowels (a, e, i, o, u) it contains.
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+string = input("Enter a string: ")
+print("Number of vowels:", count_vowels(string))
