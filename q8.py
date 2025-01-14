e86003b5c62e85db57fb45b40630c4cf26bae0fb
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:04:10 +0530

    Question 8

---
+++ q8.py
@@ -0,0 +1,4 @@
+# Convert a given temperature in Fahrenheit to Celsius and print the result.
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5.0/9.0
+print("The temperature in Celsius is:", celsius)
