44a32292cd7d9a66e4971dc222feb65033984136
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 00:03:55 +0530

    Question 7

---
+++ q7.py
@@ -0,0 +1,10 @@
+# Prompt the user for three numbers and determine which one is the largest.
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+largest = num1
+if num2 > largest:
+    largest = num2
+if num3 > largest:
+    largest = num3
+print("The largest number is:", largest)
