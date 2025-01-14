7871be086674fe8568fda19bd0ee4db0f4246fa4
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 01:27:01 +0530

    Question 41

---
+++ q41.py
@@ -0,0 +1,4 @@
+# Given a list with duplicates, create a new list with unique elements only.
+my_list = [1, 2, 3, 1, 4, 5, 2, 6]
+unique_list = list(set(my_list))
+print("List with unique elements:", unique_list)
