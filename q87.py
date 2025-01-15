91cf76f6cd291c71c930c81562f04b79ff30e015
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 23:18:24 +0530

    Question 87

---
+++ q87.py
@@ -0,0 +1,11 @@
+# Add a method greet to the Person class that prints "Hello, my name is <name>".
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        print(f"Hello, my name is {self.name}.")
+
+person = Person("Bob", 30)
+person.greet()
