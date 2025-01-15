03bdb4422dff084f957ed36062798c0459a74de7
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 23:29:40 +0530

    Question 97

---
+++ q97.py
@@ -0,0 +1,15 @@
+# Create a class Point that overloads the + operator using __add__ to add the coordinates of two Point objects.
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+p1 = Point(1, 2)
+p2 = Point(3, 4)
+print("Sum of points:", p1 + p2)
