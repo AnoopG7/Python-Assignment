1ab9f1d53ee8ffed00b33a8db3c8b10970e905c0
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-01-15 23:42:58 +0530

    Question 105

---
+++ q105.py
@@ -0,0 +1,7 @@
+# Compute mean, median, and mode of a list of numbers.
+import statistics
+
+data = [10, 20, 20, 30, 30, 30, 40]
+print("Mean:", statistics.mean(data))
+print("Median:", statistics.median(data))
+print("Mode:", statistics.mode(data))
